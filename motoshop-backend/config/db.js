e664d3938a5853a3246339e30f5cadaa7a15d5db
Gajanan Joshi <gajananpjoshi47@gmail.com>
2026-08-07 16:35:50 +0530

    Connect backend to Aiven MySQL

--- motoshop-backend/config/db.js
+++ motoshop-backend/config/db.js
@@ -1,21 +1,28 @@
-const mysql = require('mysql2')
-const dotenv = require('dotenv')
-dotenv.config()
+const mysql = require("mysql2");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    waitForConnection: true,
-    connectionlimit: 10,
-})
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
-const db = pool.promise()
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+const db = pool.promise();
 
 db.getConnection()
-    .then(()=> console.log("Mysql connected successfully"))
-    .catch((err) => console.log("Failed",err.message))
-    
+  .then(() => console.log("MySQL connected successfully"))
+  .catch((err) => console.log("Connection Failed:", err.message));
 
-module.exports = db    
+module.exports = db;
