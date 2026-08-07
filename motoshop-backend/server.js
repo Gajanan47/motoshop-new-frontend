339cabfd15608e198dddbce2bd2bdd9bbcd4912e
Gajanan Joshi <gajananpjoshi47@gmail.com>
2026-08-07 17:03:36 +0530

    slight correction for deployment

--- motoshop-backend/server.js
+++ motoshop-backend/server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(cors({
     
     origin:
-    ["http://localhost:5173", "http://127.0.0.1:5173", "http://192.168.1.43:5173"],
+    ["http://localhost:5173", "https://motoshop-backend-duct.onrender.com/"],
     credentials: true
 }))
 app.use(express.json())
